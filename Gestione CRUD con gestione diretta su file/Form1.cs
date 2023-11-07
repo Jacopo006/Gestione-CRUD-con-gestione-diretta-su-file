5ba592d204cb708d4c9efa0f45b541415f3b7c78
Riccardi.20248 <Riccardi.20248@LABI2-13PC15>
2023-11-07 11:11:31 +0100

    controllo per cancellazione fisica

--- Gestione CRUD con gestione diretta su file/Form1.cs
+++ Gestione CRUD con gestione diretta su file/Form1.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace Gestione_CRUD_con_gestione_diretta_su_file
 {
@@ -31,28 +33,70 @@ namespace Gestione_CRUD_con_gestione_diretta_su_file
 
         }
 
+        //cancellazione fisica
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int x = ricercaindice(textBox2.Text);
+            if (textBox2.Text == string.Empty)
+            {
+                MessageBox.Show("Inserisci un elemento da cancellare");
+            }
+            else if (x == -1)
+            {
+                MessageBox.Show("L'elemento non è stato trovato");
+            }
+            else
+            {
+                int indice = ricercaindice(textBox2.Text);
+                string[] linea = File.ReadAllLines("File.txt");
+                for (int i = indice; i < linea.Length - 1; i++)
+                {
+                    linea[i] = linea[i + 1];
+                }
+
+                var file = new FileStream("File.txt", FileMode.Truncate, FileAccess.Write, FileShare.Read);
+                StreamWriter sw = new StreamWriter(file);
+                sw.Write(string.Empty);
+                sw.Close();
+
+                var files = new FileStream("File.txt", FileMode.Append, FileAccess.Write, FileShare.Read);
+                StreamWriter sws = new StreamWriter(files);
+                for (int i = 0; i < linea.Length - 1; i++)
+                {
+                    sws.WriteLine(linea[i]);
+                }
+                sws.Close();
+            }
+
+        }
+
+
         //cancellazione logica
         private void cancella_Click(object sender, EventArgs e)
         {
-                int indice = ricercaindice(textBox1.Text);
-                if (indice != -1)
-                {
-                    string[] prodotto = ricercaprod(textBox1.Text);
-                    string linea;
-                    var file = new FileStream("File.txt", FileMode.Open, FileAccess.Write);
-                    BinaryWriter writer = new BinaryWriter(file);
-                    file.Seek(record * indice, SeekOrigin.Begin);
-                    linea = $"{prodotto[0]};{prodotto[1]};{prodotto[3]};1;".PadRight(record - 4) + "##";
-                    byte[] bytes = Encoding.UTF8.GetBytes(linea);
-                    writer.Write(bytes, 0, bytes.Length);
-                    writer.Close();
-                    file.Close();
-                }
-                else
-                {
-                    // Gestisci il caso in cui l'elemento non è stato trovato
-                    MessageBox.Show("L'elemento da cancellare non è stato trovato.");
-                }
+            int x = ricercaindice(textBox1.Text);
+            if (textBox1.Text == string.Empty)
+            {
+                MessageBox.Show("Inserisci un elemento da cancellare");
+            }
+            else if (x == -1)
+            {
+                MessageBox.Show("L'elemento non è stato trovato");
+            }
+            else
+            {
+                string[] prodotto = ricercaprod(textBox1.Text);
+                string linea;
+                var file = new FileStream("File.txt", FileMode.Open, FileAccess.Write);
+                BinaryWriter writer = new BinaryWriter(file);
+                file.Seek(record * x, SeekOrigin.Begin);
+                linea = $"{prodotto[0]};{prodotto[1]};{prodotto[3]};1;".PadRight(record - 4) + "##";
+                byte[] bytes = Encoding.UTF8.GetBytes(linea);
+                writer.Write(bytes, 0, bytes.Length);
+                writer.Close();
+                file.Close();
+            }
+               
         }
 
         //bottone modifica
@@ -81,37 +125,13 @@ namespace Gestione_CRUD_con_gestione_diretta_su_file
             }
         }
 
-        //cancellazione fisica
-        private void button2_Click(object sender, EventArgs e)
-        {
-            int indice = ricercaindice(textBox2.Text);
-
-            string[] linea = File.ReadAllLines("File.txt");
-            for (int i = indice; i < linea.Length - 1; i++)
-            {
-                linea[i] = linea[i + 1];
-            }
-
-            var file = new FileStream("File.txt", FileMode.Truncate, FileAccess.Write, FileShare.Read);
-            StreamWriter sw = new StreamWriter(file);
-            sw.Write(string.Empty);
-            sw.Close();
-
-            var files = new FileStream("File.txt", FileMode.Append, FileAccess.Write, FileShare.Read);
-            StreamWriter sws = new StreamWriter(files);
-            for (int i = 0; i < linea.Length - 1; i++)
-            {
-                sws.WriteLine(linea[i]);
-            }
-            sws.Close();
-        }
-
+        
 
         private void button1_Click(object sender, EventArgs e)
         {
             if (NOMEPRODOTTO.Text == string.Empty || PREZZO.Text == string.Empty)
             {
-                MessageBox.Show("Devi prima inserire un prodotto e il suo prezzo");
+                MessageBox.Show("Devi inserire il nome del prodotto e il suo prezzo");
             }
             else if (float.TryParse(PREZZO.Text, out _) == false)
             {
